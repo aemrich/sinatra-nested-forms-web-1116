4d6597d826f4c6cacda1babcc815ce5e8b839b1e
aemrich <adam.emrich@gmail.com>
2016-12-17 15:20:13 -0500

    pirates displayed

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,17 @@
 class Pirate
+  attr_accessor :name, :weight, :height
+
+  PIRATES=[]
+
+  def self.all
+    PIRATES
+  end
+
+  def initialize(params)
+    @name=params[:pirate][:name]
+    @weight=params[:pirate][:weight]
+    @height=params[:pirate][:height]
+    PIRATES << self
+  end
+  
 end
